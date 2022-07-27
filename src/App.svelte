bcff6490fdfab65cee7c2753ba496303499a6be3
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-07-27 21:52:34 +0700

    damn

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,39 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import { Router, Route, Link } from "svelte-navigator";
+  import Item from "./Item.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<Router>
+  <!--header>
+    <h1>My site</h1>
 
-  <div class="card">
-    <Counter />
-  </div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="about">About</Link>
+      <Link to="blog">Blog</Link>
+    </nav>
+  </header-->
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <main>
+    <Route path="item" component={Item} />
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+    <Route path="/">
+      <h3>Home</h3>
+      <p>Home sweet home...</p>
+			<p>
+				Check out the <Link to="/about#digital">digital heading</Link>, a heading with an <code>id </code> of “digital”
+			</p>
+    </Route>
+
+    <Route>
+      <h3>Default</h3>
+      <p>No Route could be matched.</p>
+    </Route>
+  </main>
+</Router>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	:global(:target) {
+
+	background-color: papayawhip;}
 </style>
---
+++ src/Item.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { Router, Route, Link } from "svelte-navigator";
+  //get id from url params
+  const id = new URLSearchParams(window.location.search).get("id");
+  //export let blogRoute;
+</script>
+  
+  <div class="Item">
+    <h2>{id} ไอเทม : Fino</h2>
+    <img src="https://inwfile.com/s-cb/n1gnk1.jpg" alt="">
+    <!-- owner,addtime,last see,last gps -->
+    <p>
+      ผู้ดูแล : <a href="https://www.facebook.com/profile.php?id=100009180981801">นายสมชาย สมบัติชัย</a>
+    </p>
+    <p>
+      วันที่เพิ่ม : <time datetime="2019-01-01">2019-01-01</time>
+    </p>
+    <p>
+      วันที่อัพเดทล่าสุด : <time datetime="2019-01-01">2019-01-01</time>
+    </p>
+    <!-- button แจ้งหาย,อัปเดตที่อยู่ล่าสุด -->
+    <p>
+      <a href="https://www.facebook.com/profile.php?id=100009180981801">
+        <button>แจ้งหาย</button>
+      </a>
+      <a href="https://www.facebook.com/profile.php?id=100009180981801">
+        <button>อัปเดตที่อยู่ล่าสุด</button>
+      </a>
+    </p>
+  </div>  
