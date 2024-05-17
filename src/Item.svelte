49a115841f94d352afbde32fbf5d9f89a60ded1c
Phongsakorn Wisetthon <boy1556@hotmail.com>
2024-05-17 21:31:59 +0700

    refactor: Update URL handling in Item.svelte to fix navigation issue

--- src/Item.svelte
+++ src/Item.svelte
@@ -7,27 +7,52 @@
     return c;
   }
 </script>
-  
-  <div class="Item">
+
+<div class="Item">
+  {#if getParam('id') == 'nothingear'}
+    <h2>สิ่งของ : Nothing Ear</h2>
+      <img src="https://inwfile.com/s-cb/n1gnk1.jpg" alt="">
+      <!-- owner,addtime,last see,last gps -->
+      <p>
+        เจ้าของ : นายพงศกร วิเศษธร
+      </p>
+      <p>
+        วันที่เพิ่ม : <time datetime="2024-05-17">2024-05-17</time>
+      </p>
+      <p>
+        วันที่อัพเดทล่าสุด : <time datetime="2024-05-17">2024-05-17</time>
+      </p>
+      <!-- button แจ้งหาย,อัปเดตที่อยู่ล่าสุด -->
+      <p>
+        <a href="https://www.facebook.com/profile.php?id=100009180981801">
+          <button>แจ้งหาย</button>
+        </a>
+        <!-- <a href="https://www.facebook.com/profile.php?id=100009180981801">
+          <button>อัปเดตที่อยู่ล่าสุด</button>
+        </a> -->
+      </p>
+  {:else}
     <h2>{getParam('id')} ไอเทม : Fino</h2>
-    <img src="https://inwfile.com/s-cb/n1gnk1.jpg" alt="">
-    <!-- owner,addtime,last see,last gps -->
-    <p>
-      ผู้ดูแล : <a href="https://www.facebook.com/profile.php?id=100009180981801">นายสมชาย สมบัติชัย</a>
+      <img src="https://inwfile.com/s-cb/n1gnk1.jpg" alt="">
+      <!-- owner,addtime,last see,last gps -->
+      <p>
+        ผู้ดูแล : <a href="https://www.facebook.com/profile.php?id=100009180981801">นายสมชาย สมบัติชัย</a>
+      </p>
+      <p>
+        วันที่เพิ่ม : <time datetime="2019-01-01">2019-01-01</time>
+      </p>
+      <p>
+        วันที่อัพเดทล่าสุด : <time datetime="2019-01-01">2019-01-01</time>
+      </p>
+      <!-- button แจ้งหาย,อัปเดตที่อยู่ล่าสุด -->
+      <p>
+        <a href="https://www.facebook.com/profile.php?id=100009180981801">
+          <button>แจ้งหาย</button>
+        </a>
+        <!-- <a href="https://www.facebook.com/profile.php?id=100009180981801">
+          <button>อัปเดตที่อยู่ล่าสุด</button>
+        </a> -->
     </p>
-    <p>
-      วันที่เพิ่ม : <time datetime="2019-01-01">2019-01-01</time>
-    </p>
-    <p>
-      วันที่อัพเดทล่าสุด : <time datetime="2019-01-01">2019-01-01</time>
-    </p>
-    <!-- button แจ้งหาย,อัปเดตที่อยู่ล่าสุด -->
-    <p>
-      <a href="https://www.facebook.com/profile.php?id=100009180981801">
-        <button>แจ้งหาย</button>
-      </a>
-      <!-- <a href="https://www.facebook.com/profile.php?id=100009180981801">
-        <button>อัปเดตที่อยู่ล่าสุด</button>
-      </a> -->
-    </p>
-  </div>  
+  {/if}
+</div>
+  
